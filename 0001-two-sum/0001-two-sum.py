b2b912b64c1756023975007f0a4b114f098dddd7
Mohamed Badr <60277974+Mohamed-A-Badr@users.noreply.github.com>
2023-04-15 00:59:36 +0200

    Time: 53 ms (96.87%), Space: 15.3 MB (10.42%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -2,14 +2,10 @@ from collections import defaultdict
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        mp = defaultdict(list)
-        idx = 0
+        mp = dict()
         for idx, x in enumerate(nums):
-            mp[x].append(idx)
-
-        for idx, i in enumerate(nums):
-            x = target - i
-            for j in mp[x]:
-                if j == idx:
-                    continue
-                return [idx, j]
+            if x in mp:
+                return [idx, mp[x]]
+            else:
+                mp[target-x] = idx
+                
