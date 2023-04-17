f2bd8f917dd68d8a86dac8cfe797dc69b0a14e0a
Mohamed Badr <60277974+Mohamed-A-Badr@users.noreply.github.com>
2023-04-17 05:10:14 +0200

    Time: 327 ms (73.03%), Space: 28.9 MB (86.95%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -3,7 +3,6 @@ class Solution:
         if len(nums) == 0 or len(nums) == 1:
             return len(nums)
         st = sorted(list(set(nums)))
-        print(st)
         cnt, res = 1, 1
         for i in range(1, len(st)):
             if st[i] - st[i - 1] == 1:
